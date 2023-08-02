8b715bfd7dd2bd588908d20c584d3aca77da14e3
Dylan <dgrinton@live.ca>
2023-08-01 22:04:49 -0400

    Add jsdoc comments

--- include/ObjectUtility.hpp
+++ include/ObjectUtility.hpp
@@ -4,9 +4,12 @@
 #include "PolyCollider.hpp"
 
 namespace ObjectUtil {
+    /** Get axis-alligned bounding box collider with given dimensions */
     PolyCollider *GetBoxCollider(const float &width, const float &height, const float &depth);
 
+    /** Get Axis-alligned bounding box with given dimensions */
     Body *GetBox(const float &width, const float &height, const float &depth);
 
+    /** Get Axis-alligned bounding box with given dimensions*/
     Rigidbody *GetRigidBox(const float &width, const float &height, const float &depth);
 }
