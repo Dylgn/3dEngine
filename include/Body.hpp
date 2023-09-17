f7663e6df8f141f67b0af6593392e27b844c101e
Dylan <dgrinton@live.ca>
2023-09-17 00:47:49 -0400

    Update old physics body class

--- include/Body.hpp
+++ include/Body.hpp
@@ -16,3 +16,27 @@ class Body {
         
         friend class Object;
 };
+
+class Rbody: public Body {
+    V3d force;  
+    V3d velocity;
+    float mass = 1.0f;
+    public:
+        Rbody(Collider *collider);
+        /** Calculates velocity direction */
+        V3d CalculateVelocity(const float &elapsed_time);
+        /** Set velocity */
+        void SetVelocity(const V3d &dir);
+        /** Get velocity */
+        V3d GetVelocity();
+        /** Accelerates body in given direction (F=ma)*/
+        void Accelerate(const V3d &dir);
+        /** Applies force to body */
+        void ApplyForce(const V3d &dir);
+        /** Sets force of body */
+        void SetForce(const V3d &dir);
+        /** Get force */
+        V3d GetForce();
+        /** Get mass */
+        float GetMass();
+};
---
+++ src/Physics/Body.cpp
@@ -0,0 +1,36 @@
+#include "Body.hpp"
+
+Rbody::Rbody(Collider *collider): Body{collider} {}
+
+V3d Rbody::CalculateVelocity(const float &elapsed_time) {
+    velocity += (force / mass) * elapsed_time;
+    return velocity;
+}
+
+void Rbody::SetVelocity(const V3d &dir) {
+    velocity = dir;
+}
+
+V3d Rbody::GetVelocity() {
+    return velocity;
+}
+
+void Rbody::Accelerate(const V3d &dir) {
+    force += dir * mass;
+}
+
+void Rbody::ApplyForce(const V3d &dir) {
+    force += dir;
+}
+
+void Rbody::SetForce(const V3d &dir) {
+    force = dir;
+}
+
+V3d Rbody::GetForce() {
+    return force;
+}
+
+float Rbody::GetMass() {
+    return mass;
+}
