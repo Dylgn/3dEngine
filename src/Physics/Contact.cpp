1fd55b637ff9fe76a388a5e077c79471ecc9b8fb
Dylan <dgrinton@live.ca>
2024-09-21 18:39:45 -0400

    Remove old physics engine

--- src/Physics/Contact.cpp
+++
@@ -1,8 +0,0 @@
-#include "Contact.hpp"
-
-void Contact::SetBodyData(Rigidbody *one, Rigidbody *two, float friction, float restitution) {
-    body[0] = one;
-    body[1] = two;
-    this->friction = friction;
-    this->restitution = restitution;
-}
--- src/Physics/GravityGenerator.cpp
+++
@@ -1,8 +0,0 @@
-#include "GravityGenerator.hpp"
-
-GravityGenerator::GravityGenerator(const V3d &gravity): gravity{gravity} {}
-
-void GravityGenerator::UpdateForce(Particle *p, float elapsed_time) {
-    if (p->GetInverseMass() == 0) return; // Infinite mass
-    else p->ApplyForce(gravity * p->GetMass());
-}
